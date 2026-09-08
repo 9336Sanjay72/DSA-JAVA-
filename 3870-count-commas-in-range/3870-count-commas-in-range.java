05c54937711c5751a6e7f7eb5c8ab30da8ad8413
sanjay panchal <sanjaypanchal0975@gmail.com>
2026-09-08 12:02:01 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (39.85%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        String s=String.valueOf(n);
+         int count=0;
+        if(s.length()<4)return 0;  
+        else{
+            count=n-999;
+        }
+        return count;
+    }
+}
