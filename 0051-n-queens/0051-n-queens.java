1791022341491dca586feedf1c8d959ef2ceee28
sanjay panchal <sanjaypanchal0975@gmail.com>
2026-09-09 18:37:44 +0530

    Time: 2 ms (81.35%), Space: 46.6 MB (46.02%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,39 +1,40 @@
 class Solution {
-    public List<List<String>>  result=new ArrayList<>();
-    public List<String> build(int n,List<Integer> list){
-        List<String> board=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            char[]arr=new char[n];
-            Arrays.fill(arr,'.');
-            arr[list.get(i)]='Q';
-            board.add(new String(arr));
+    public List<List<String>> result=new ArrayList<>();
+    public boolean isSafe(int row,int col,int n,char[][]board){
+        for(int i=row-1;i>=0;i--){// ussse ek phele row mai jakar col check karna 
+            if(board[i][col]=='Q')return false;
         }
-        return board;
-    }
-    public boolean isValid(int row,int col,List<Integer> list){
-        // check for previous col
-        for(int prevRow=0;prevRow<list.size();prevRow++){
-            int prevCol=list.get(prevRow);
-            if(prevCol==col)return false;
-            if(Math.abs(prevCol-col)==Math.abs(prevRow-row))return false;
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q')return false;// left diagonal ke liye
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q')return false;// right daigonal
         }
         return true;
     }
-    public void solve(int n,int row,List<Integer> temp){
-        if(row==n){
-            result.add(build(n,temp));
+    public void solve(int row,int n,char[][]board){
+        if(row==n){   
+            List<String> temp=new ArrayList<>();      
+            for(int i=0;i<n;i++){
+              temp.add(new String (board[i]));
+            }
+            result.add(temp);
             return;
         }
         for(int col=0;col<n;col++){
-            if(isValid(row,col,temp)){
-                temp.add(col);
-                solve(n,row+1,temp);
-                temp.remove(temp.size()-1);
+            if(isSafe(row,col,n,board)){
+                board[row][col]='Q';
+                solve(row+1,n,board);
+                board[row][col]='.';
             }
         }
     }
     public List<List<String>> solveNQueens(int n) {
-        solve(n,0,new ArrayList<>());// for 0th row
+        char [][]board=new char[n][n];
+        for(char []ch: board){
+            Arrays.fill(ch,'.');
+        }
+        solve(0,n,board);
         return result;
     }
 }
