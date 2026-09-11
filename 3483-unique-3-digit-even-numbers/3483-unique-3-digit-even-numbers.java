648099314c64e676c554b46b7d8452728f546f18
sanjay panchal <sanjaypanchal0975@gmail.com>
2026-09-11 19:13:37 +0530

    Time: 3 ms (89.07%), Space: 45.4 MB (79.6%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -20,17 +20,44 @@
 
 class Solution {
     public int totalNumbers(int[] digits) {
-       Set<Integer> set=new HashSet<>();
-       for(int i=0;i<digits.length;i++){
-        for(int j=0;j<digits.length;j++){
-            for(int k=0;k<digits.length;k++){
-                    if(i==j || j==k || i==k)continue;
-                    if(digits[i]!=0 && digits[k]%2==0 ){
-                        int num=digits[i]*100+digits[j]*10+digits[k];
-                        set.add(num);
-                    }
+    //    Set<Integer> set=new HashSet<>();
+    //    for(int i=0;i<digits.length;i++){
+    //     for(int j=0;j<digits.length;j++){
+    //         for(int k=0;k<digits.length;k++){
+    //                 if(i==j || j==k || i==k)continue;
+    //                 if(digits[i]!=0 && digits[k]%2==0 ){
+    //                     int num=digits[i]*100+digits[j]*10+digits[k];
+    //                     set.add(num);
+    //                 }
+    //         }
+    //     }
+    //    }
+    //    return set.size();
+
+    Set<Integer> set=new HashSet<>();
+    int[]freq=new int[10];// freq to store the frequency of all element
+    for(int num : digits){
+        freq[num]++;
+    }
+       for(int i=1;i<=9;i++){// first digit can be from 1 to 9
+        // suppose we have duplicate element 2 we have used it for 1st digit so we reduce its frequency so that we cannot that particular element of particular index
+        if(freq[i]==0)continue;
+        else freq[i]--;
+        for(int j=0;j<=9;j++){//second digit can be from 0 to 9 
+            // similiar for this 
+            if(freq[j]==0)continue;
+            else freq[j]--;
+            for(int k=0;k<=8;k+=2){// last digit can only be 0 ,2,4,6,8
+                    if(freq[k]==0)continue;
+                    else freq[k]--;
+                    int num=i*100+j*10+k;
+                    set.add(num);
+                    freq[k]++;// suppose we have 3 times 2 digits suppose we have have used it for last digit and frequency became zero but we can also use 2 as 1st digit that's why we revive its frequency not only for this loop for 2nd and 1st digit too
+                    
             }
+            freq[j]++;
         }
+        freq[i]++;
        }
        return set.size();
     }
