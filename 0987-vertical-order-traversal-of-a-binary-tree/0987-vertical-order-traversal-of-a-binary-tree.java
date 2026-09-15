f1c54aa95359f985cf456b9bfb8ed752c5c974e6
sanjay panchal <sanjaypanchal0975@gmail.com>
2026-09-15 19:11:38 +0530

    Time: 3 ms (94.18%), Space: 43.8 MB (85.52%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -75,65 +75,110 @@
 // }
 
 
-class Node {
-    TreeNode root;
-    int row;
-    int col;
+// class Node {
+//     TreeNode root;
+//     int row;
+//     int col;
 
-    Node(TreeNode root, int row, int col) {
-        this.root = root;
-        this.row = row;
-        this.col = col;
+//     Node(TreeNode root, int row, int col) {
+//         this.root = root;
+//         this.row = row;
+//         this.col = col;
+//     }
+// }
+
+// class Solution {
+
+//     public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+//         List<Node> list = new ArrayList<>();
+
+//         solve(root, 0, 0, list);
+
+//         // Sort by column, then row, then value
+//         Collections.sort(list, (a, b) -> {
+
+//             if (a.col != b.col)
+//                 return a.col - b.col;
+
+//             if (a.row != b.row)
+//                 return a.row - b.row;
+
+//             return a.root.val - b.root.val;
+//         });
+
+//         List<List<Integer>> ans = new ArrayList<>();
+//         List<Integer> current = new ArrayList<>();
+
+//         int prevCol = list.get(0).col;
+
+//         for (Node node : list) {
+
+//             if (node.col != prevCol) {
+//                 ans.add(current);
+//                 current = new ArrayList<>();
+//                 prevCol = node.col;
+//             }
+
+//             current.add(node.root.val);
+//         }
+
+//         ans.add(current);
+
+//         return ans;
+//     }
+
+//     public void solve(TreeNode root, int row, int col, List<Node> list) {
+//         if (root == null)
+//             return;
+//         list.add(new Node(root, row, col));
+
+//         solve(root.left, row + 1, col - 1, list);
+//         solve(root.right, row + 1, col + 1, list);
+//     }
+// }
+
+class Node{
+    int val, row,col;
+    Node(int val,int row,int col){
+        this.val=val;
+        this.row=row;
+        this.col=col;
     }
 }
-
 class Solution {
-
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-
-        List<Node> list = new ArrayList<>();
-
-        solve(root, 0, 0, list);
-
-        // Sort by column, then row, then value
-        Collections.sort(list, (a, b) -> {
-
-            if (a.col != b.col)
-                return a.col - b.col;
-
-            if (a.row != b.row)
-                return a.row - b.row;
-
-            return a.root.val - b.root.val;
-        });
-
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-
-        int prevCol = list.get(0).col;
-
-        for (Node node : list) {
-
-            if (node.col != prevCol) {
-                ans.add(current);
-                current = new ArrayList<>();
-                prevCol = node.col;
-            }
-
-            current.add(node.root.val);
-        }
-
-        ans.add(current);
-
-        return ans;
+    public List<Node> list=new ArrayList<>();
+    public List<List<Integer>> result=new ArrayList<>();
+    public void solve(TreeNode root,int row,int col){
+        if(root==null)return;
+        list.add(new Node(root.val,row,col));
+        solve(root.left,row+1,col-1);
+        solve(root.right,row+1,col+1);
     }
+    public void sort(){
+        Collections.sort(list,(a,b)->{
+            if(a.col!=b.col)return a.col-b.col;
+            if(a.row!=b.row)return a.row-b.row;
+            return a.val-b.val;
+        });
+    }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        if(root==null)return result;
+        solve(root,0,0);
+        sort();
+        List<Integer> temp=new ArrayList<>();// temp list to build result array like if having col -1,-1 -1 then we put these in single list thats why we are using it 
+        int prevCol=list.get(0).col;// dusri nayi banani ke kaam mai aayega
 
-    public void solve(TreeNode root, int row, int col, List<Node> list) {
-        if (root == null)
-            return;
-        list.add(new Node(root, row, col));
+        for(Node node : list){
+            if(node.col!=prevCol){// col:   -1  -1   0   0   1   1 suppose these are columns first prevCol is on -1 and when node col became 0 it means we have to build new temp list to store in result arraylist
+                result.add(temp);// purani list add kar deni hai fir fresh new arrayList banayi hai 
+                temp=new ArrayList<>();
+                prevCol=node.col;
 
-        solve(root.left, row + 1, col - 1, list);
-        solve(root.right, row + 1, col + 1, list);
+            }
+            temp.add(node.val);// when prevcol=current col  -1  -1 
+        }
+        result.add(temp);// akhiri list add karne ke liye
+        return result;
     }
 }
